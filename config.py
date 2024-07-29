c927ff64968c991d63bf0db6d9c0b4aa5185a6be
Itzik Waizman <itzikw96@gmail.com>
2024-07-29 12:48:21 +0300

    Title: Config files
    Description: Support receiving json config files as inputs. Also, upload
    	     ready config files for primal and dual simulations with different alpha
                 values.

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 import random
+import json
 
 class Parameters:
-    def __init__(self):     
+    def __init__(self, config_path=None):
         self.params = dict()
 
+        if config_path:
+            self.load_params_from_file(config_path)
+        else:
+            self.set_default_params()
+
+    def set_default_params(self):
         """ Network Topology Parameters """
-
-        # nodes - List of non negative integers, each represent a node id in the network.
         self.params['nodes'] = [0, 1, 2, 3, 4, 5]
-
-        # links - Dictionary containing the nodes each link connects and the corresponding capacity.
         self.params['links'] = [{'node1': 0, 'node2': 1, 'capacity': 1},
                                 {'node1': 1, 'node2': 2, 'capacity': 1},
                                 {'node1': 2, 'node2': 3, 'capacity': 1},
                                 {'node1': 3, 'node2': 4, 'capacity': 1},
                                 {'node1': 4, 'node2': 5, 'capacity': 1}]
-        
-        # users - Dictionary containing each user id, source node, destination node, and the path between these nodes.
-        # Note that a path cannot contain links that do not exists in params['links'].
         self.params['users'] = [
             {'user_id': 0, 'source': 0, 'destination': 5, 'path': [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]},
             {'user_id': 1, 'source': 0, 'destination': 1, 'path': [(0, 1)]},
@@ -26,30 +26,20 @@ class Parameters:
             {'user_id': 4, 'source': 3, 'destination': 4, 'path': [(3, 4)]},
             {'user_id': 5, 'source': 4, 'destination': 5, 'path': [(4, 5)]}
         ]
-
-        """ Simulation Parameters """
-
-        # alpha - Nonegative number for the alpha-fairness criterion.
-        self.params['alpha'] = 50
-
-        # iterations - Integer describing the upper limit of the number of iterations.
-        self.params['iterations'] = 20000
-
-        # step_size - Float describing the step size in gradient descent/ascent algorithms.
-        self.params['step_size'] = 5e12 #5e-1
-
-        # algorithm - String indicating the type of algorithm. Supported algorithms are 'primal' and 'dual'.
+        self.params['shortest_path_algorithm'] = 'dijkstra'
+        self.params['alpha'] = 1
+        self.params['iterations'] = 200
+        self.params['step_size'] = 5e-1
         self.params['algorithm'] = 'dual'
-
-        """" Primal algorithm parameters """
-        
-        # normalization_factor - Float. Constant to normalize barrier function values in the feasible region.
         self.params['normalization_factor'] = 4e-6
-        
         self.params['beta'] = 20
-
         self.params['max_rate_update'] = 0.1
-
-        """ Dual algorithm parameters """
-        
         self.params['rate_upper_bound'] = 100
+
+    def load_params_from_file(self, config_path):
+        with open(config_path, 'r') as file:
+            self.params = json.load(file)
+
+    def save_params_to_file(self, config_path):
+        with open(config_path, 'w') as file:
+            json.dump(self.params, file, indent=4)
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import argparse
+import os
 from config import Parameters
 from simulation import Simulator
 
-def main():
+def main(config_path=None):
     # Create an instance of Parameters
-    Params = Parameters()
+    Params = Parameters(config_path)
     
     # Create an instance of Simulator using the parameters
     simulator = Simulator(Params.params)
     
     # Print the simulation details
-    simulator.print_simulation_details()
+    #simulator.print_simulation_details()
     
     # Visualize the network to check if it initializes correctly
     simulator.run_simulation()
     
     # Initialize and run the algorithm specified in the parameters
     simulator.post_simulation()
+
+    Params.save_params_to_file('simulations_config_files/dual_alpha1.json')
+
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Network Simulator')
+    parser.add_argument('--config', type=str, help='Path to the config file')
+    args = parser.parse_args()
+
+    config_path = args.config if args.config else None
+    main(config_path)
